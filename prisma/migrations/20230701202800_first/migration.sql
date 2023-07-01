69966fc5a4ea14a2a69391eaf9283f3b980b8065
TBenjaminL90 <tomasbenjamin.l90@gmail.com>
2023-07-01 17:29:16 -0300

    update

--- prisma/migrations/20230701202800_first/migration.sql
+++ prisma/migrations/20230701202800_first/migration.sql
@@ -22,3 +22,17 @@ CREATE TABLE `memories` (
     INDEX `memories_userId_idx`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `tasks` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(120) NOT NULL,
+    `description` TEXT NOT NULL,
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    INDEX `tasks_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
