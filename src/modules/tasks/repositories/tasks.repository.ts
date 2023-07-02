d5ee17409c6a94b38daeb8e201b496c256922df3
TBenjaminL90 <tomasbenjamin.l90@gmail.com>
2023-07-02 17:52:18 -0300

    fix import

--- src/modules/tasks/repositories/tasks.repository.ts
+++ src/modules/tasks/repositories/tasks.repository.ts
@@ -1,6 +1,6 @@
-import { UpdateTaskDto } from 'tasks/dto/update-task.dto'
 import { Task } from '../entities/task.entity'
 import { CreateTaskDto } from '../dto/create-task.dto'
+import { UpdateTaskDto } from '../dto/update-task.dto'
 
 export abstract class TasksRepository {
   abstract create(data: CreateTaskDto, userId: string): Promise<Task>
